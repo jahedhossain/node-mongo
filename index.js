eca7dd6d6348b39bed5cb1fd23cfbc532ad96468
jahed <jahed.w.dev@gmail.com>
2020-03-29 17:17:21 +0600

    06-create POST api, install postman, read post data, use body parser

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(cors());
-
-app.get("/", (req, res) => res.send("Hellofaf World!"));
-
-app.get("/users", (req, res) => {
-  res.send([
-    {
-      name: "jahid",
-      age: 10,
-      hobby: "football"
-    },
-    {
-      name: "tarek",
-      age: 20,
-      hobby: "cricket"
-    }
-  ]);
-});
+app.use(bodyParser.json());
 
 const users = ["jahed", "tarek", "nuruddin", "romjan"];
 app.get("/user/:id", (req, res) => {
   const id = req.params.id;
   const name = users[id];
   res.send({ id, name });
+});
+
+app.post("/addUser", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(3000, () => console.log("my sever port: 3000"));
