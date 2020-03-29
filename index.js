b4491455fd1a55ab7aaecae8361ee03b7b57f572
jahed <jahed.w.dev@gmail.com>
2020-03-29 15:09:59 +0600

    03-install nodemon, api return object, create multiple api

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hellofaf World!"));
+app.get("/users", (req, res) => {
+  res.send([
+    {
+      name: "jahid",
+      age: 10,
+      hobby: "football"
+    },
+    {
+      name: "tarek",
+      age: 20,
+      hobby: "cricket"
+    }
+  ]);
+});
 
 app.listen(3000, () => console.log("my sever port: 3000"));
