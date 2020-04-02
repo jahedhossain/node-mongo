7ba38a8c09ed53849190fe775b1e85d770bad311
jahed <jahed.w.dev@gmail.com>
2020-04-02 10:20:41 +0600

    db connection, load database data, dynamic data entry

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
+// middleware
 app.use(cors());
 app.use(bodyParser.json());
+const uri = "mongodb://localhost:27017/myproject";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
+// Get users route
 const users = ["jahed", "tarek", "nuruddin", "romjan"];
 app.get("/user/:id", (req, res) => {
   const id = req.params.id;
@@ -13,8 +18,26 @@ app.get("/user/:id", (req, res) => {
   res.send({ id, name });
 });
 
-app.post("/user", (req, res) => {
-  res.send(req.body);
+// Post user email and password route
+app.post("/users", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  client.connect(err => {
+    const collection = client.db("onlineStore").collection("users");
+    collection.insertOne(user, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({
+          error: "database inside error",
+          message: err
+        });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+    console.log("database connect.......haa");
+    // client.close();
+  });
 });
 
 app.listen(3000, () => console.log("my sever port: 3000"));
